c6d788cf6d7c9f08357bf84795e65bb16d9727bf
Pravoznavets <158168143+Pravoznavets@users.noreply.github.com>
2024-01-30 13:16:15 +0000

    Deploying to webpage from @ Pravoznavets/pravoznavets@0db1781ddc901011118fb0792f7801be0ef20610 🚀

---
+++ static/js/557.f1f81e2e.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkpravoznavets=self.webpackChunkpravoznavets||[]).push([[557],{557:(e,a,s)=>{s.r(a),s.d(a,{default:()=>t});var n=s(184);const t=()=>(0,n.jsx)("div",{children:"NotFoundPage"})}}]);
+//# sourceMappingURL=557.f1f81e2e.chunk.js.map
